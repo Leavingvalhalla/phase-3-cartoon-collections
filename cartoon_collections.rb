7bd67e9876900b8848183211ecd097e53800de85
Zach Lefler <leaving.valhalla@gmail.com>
2022-04-06 10:05:40 -0700

    commit tree b7c99d2d4c46084f0552ab02f12840ff9eafc1b3

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -14,24 +14,33 @@ def long_planeteer_calls (words)
   words.filter {|word| word.length > 4} == [] ? false : true
 end
 
-def find_the_cheese (array)
+# def find_the_cheese (array)
+#   cheese_types = ["cheddar", "gouda", "camembert"]
+#   first_cheese = ''
+#   cheese_index = array.length
+#   cheese_types.each do |cheese|
+#     binding.pry
+#   if array.include? cheese && (array.find_index(cheese) < cheese_index)
+#     puts true
+#     first_cheese = cheese
+#     cheese_index = array.find_index(cheese)
+#     if first_cheese != ''
+#       first_cheese
+#     else
+#     nil
+#     end
+#     end
+#   end
+# end
+
+def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
-  first_cheese = ''
-  cheese_index = array.length
-  cheese_types.each do |cheese|
-    binding.pry
-  if array.include? cheese && array.find_index(cheese) < cheese_index
-    puts true
-    first_cheese = cheese
-    cheese_index = array.find_index(cheese)
-    if first_cheese != ''
-      first_cheese
-    else
-    nil
-      end
-    end
+  array.find do |first_cheese|
+    cheese_types.include? first_cheese
   end
 end
+
+
 
 soup = ["tomato soup", "cheddar", "oyster crackers", "gouda"]
 find_the_cheese(soup)
