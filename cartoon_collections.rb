e393a3c5da8e1213220402596c13eb573228ae19
Zach Lefler <leaving.valhalla@gmail.com>
2022-04-06 09:44:07 -0700

    initial commit

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,37 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+require 'pry'
+
+def roll_call_dwarves (dwarves)
+  dwarves.each_with_index do |dwarf, i| 
+  puts "#{i + 1}. #{dwarf}"
+  end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet (calls)
+  calls.map {|call| call.capitalize + "!"}
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls (words)
+  words.filter {|word| word.length > 4} == [] ? false : true
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese (array)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  first_cheese = ''
+  cheese_index = array.length
+  cheese_types.each do |cheese|
+    binding.pry
+  if array.include? cheese && array.find_index(cheese) < cheese_index
+    puts true
+    first_cheese = cheese
+    cheese_index = array.find_index(cheese)
+    if first_cheese != ''
+      first_cheese
+    else
+    nil
+      end
+    end
+  end
 end
+
+soup = ["tomato soup", "cheddar", "oyster crackers", "gouda"]
+find_the_cheese(soup)
